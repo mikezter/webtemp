f90a61a23cb76055783186627b0dc18ff0c60dd0
Michael Strüder <mikezter@gmail.com>
2013-10-06 06:46:01 +0200

    ard alias

---
+++ ardenv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export ARD_HOME=/Applications/Arduino.app/Contents/Resources/Java
+export AVR_HOME=$ARD_HOME/hardware/tools/avr
+export ARD_BIN=$AVR_HOME/bin
+export AVRDUDE=$ARD_BIN/avrdude
+export AVRDUDE_CONF=$AVR_HOME/etc/avrdude.conf
